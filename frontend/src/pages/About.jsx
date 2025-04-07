ea6a87ee06b5c247c12cb77a342be151a5881647
ADNANLH <adnanharrak5@gmail.com>
2025-04-07 17:11:19 +0100

    test

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -18,6 +18,7 @@ const About = () => {
             people shop online. Our journey began with a simple idea: to provide a platform where
             customers can easily discover, explore, and purchase a wide range of products from the
             comfort of their homes.</p>
+            
           <p>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality
             products that cater to every taste and preference. From fashion and beauty to
             electronics and home essentials, we offer an extensive collection sourced from trusted
